a265bb86414a9ab64acfcc3b748b8998776dfa82
Feras Jobeir <fjobeir@gmail.com>
2023-01-03 16:46:48 +0300

    Understanding Migrations

---
+++ migrations/20230103131644-add_admin_phone_field.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Admins', 'phone', { 
+      type: Sequelize.STRING(20)
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Admins', 'phone')
+  }
+};
---
+++ migrations/20230103133233-change-admin-phone-length.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.changeColumn('Admins', 'phone', {
+      type: Sequelize.STRING(18),
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.changeColumn('Admins', 'phone', {
+      type: Sequelize.STRING(20),
+    });
+  }
+};
