21d81035b78f45f933f32481e1c8b813ef7af0ac
Feras Jobeir <fjobeir@gmail.com>
2023-01-05 12:41:39 +0300

    Show Province + Postman Collection

--- controllers/provinceController.js
+++ controllers/provinceController.js
@@ -43,24 +43,17 @@ const index = async (req, res, next) => {
 }
 const show = async (req, res, next) => {
     const result = {
-        success: true,
+        success: false,
         data: null,
         messages: []
     }
-    const id = +req.params.id
-    if (typeof id === 'number' && id > 0) {
-        const province = await models.Province.findByPk(id)
-        result.data = province
-        if (!province) {
-            res.status(404)
-            result.success = false
-            result.messages.push('Province not found')
-        }
-    } else {
-        res.status(422)
-        result.success = false
-        result.messages.push('Please provide a valid id')
+    const item = await getInstanceById(req.params.id, 'Province')
+    if (item.success) {
+        result.success = true
+        result.data = item.instance
     }
+    result.messages = [...item.messages]
+    res.status(item.status)
     return res.send(result)
 }
 const update = async (req, res, next) => {
@@ -70,7 +63,7 @@ const update = async (req, res, next) => {
         messages: []
     }
     const { name = '' } = req.body
-    const item = getInstanceById(req.params.id, 'Province')
+    const item = await getInstanceById(req.params.id, 'Province')
     if (item.success) {
         if (!validateName(name)) {
             result.messages.push('Please enter a valid province name')
@@ -85,6 +78,7 @@ const update = async (req, res, next) => {
     } else {
         result.messages = [...item.messages]
     }
+    
     res.status(item.status)
     return res.send(result)
 }
